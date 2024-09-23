65209d836de5e0fdd7d7df1368125b2d4577237c
Choudhary Himanshu <himanshuch3003@gmail.com>
2024-09-23 18:13:52 +0530

    Pushing FrontEnd

--- chatappclient/src/App.js
+++ chatappclient/src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [input, setInput] = useState('');
   const messagesEndRef = useRef(null);
   useEffect(() => {
-    const ws = new WebSocket(process.env.REACT_APP_API_URL);
+    const ws = new WebSocket("https://anonymouschatapp-1.onrender.com");
     setSocket(ws);
     ws.onmessage = (event) => {
       const newMessage = event.data;
